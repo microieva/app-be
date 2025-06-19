016fcca87ca25d8f323036f3e122bf183faf0cca
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2025-06-19 13:56:37 +0300

    delete socket.ts

--- src/server/socket/handlers.ts
+++ src/server/socket/handlers.ts
@@ -5,11 +5,12 @@ export const activeDoctors = new Map();
 export const handleConnection = (io: Server, socket: Socket) => {
     console.log('New client connected:', socket.id);
 
-    socket.on('join', (user) => {
+    socket.on('join_room', (user) => {
         if (user?.id && user?.userRole) {
             const personalRoom = `${user.userRole}_${user.id}`;
             const roleRoom = `${user.userRole}s`;
             socket.join([personalRoom, roleRoom]);
+            console.log('user joined ', personalRoom, roleRoom)
         }
     });
 
