ba817348fa9ba2dc08f5b0ddc6f8e5ba4dff936c
Ieva Vyliaudaite <ievavyliaudaite@Ieva-MacBook-Pro.local>
2025-06-25 15:43:42 +0300

    modify config folder structure

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,6 +1,6 @@
 require('dotenv');
-import { devDataSource } from '../configurations/dev-db.config';
-import { prodDataSource } from '../configurations/prod-db.config';
+import { devDataSource } from './db-config/dev-db.config';
+import { prodDataSource } from './db-config/prod-db.config';
 
 export const getDataSource = () => 
     process.env.NODE_ENV === 'production' ? prodDataSource : devDataSource;
